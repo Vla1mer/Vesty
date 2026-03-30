097424c9cb6935768334ff89b3ff26e82814ad8a
Vla1mer <vla1mer8@gmail.com>
2026-03-30 13:22:29 +0200

    Added Lazy

--- internship/Repository/RepositoryManager.cs
+++ internship/Repository/RepositoryManager.cs
@@ -2,28 +2,27 @@
 
 namespace internship.Repository
 {
-    public class RepositoryManager : IRepositoryManager
+    public sealed class RepositoryManager : IRepositoryManager
     {
         private readonly AppDbContext _context;
-        private IUserRepository? _userRepository;
-        private IChatRepository? _chatRepository;
-        private IChatMemberRepository? _chatMemberRepository;
-        private IMessageRepository? _messageRepository;
+        private readonly Lazy<IUserRepository> _userRepository;
+        private readonly Lazy<IChatRepository> _chatRepository;
+        private readonly Lazy<IChatMemberRepository> _chatMemberRepository;
+        private readonly Lazy<IMessageRepository> _messageRepository;
 
         public RepositoryManager(AppDbContext context)
-            => _context = context;
+        {
+            _context = context;
+            _userRepository = new Lazy<IUserRepository>(() => new UserRepository(context));
+            _chatRepository = new Lazy<IChatRepository>(() => new ChatRepository(context));
+            _chatMemberRepository = new Lazy<IChatMemberRepository>(() => new ChatMemberRepository(context));
+            _messageRepository = new Lazy<IMessageRepository>(() => new MessageRepository(context));
+        }
 
-        public IUserRepository User =>
-            _userRepository ??= new UserRepository(_context);
-
-        public IChatRepository Chat =>
-            _chatRepository ??= new ChatRepository(_context);
-
-        public IChatMemberRepository ChatMember =>
-            _chatMemberRepository ??= new ChatMemberRepository(_context);
-
-        public IMessageRepository Message =>
-            _messageRepository ??= new MessageRepository(_context);
+        public IUserRepository User => _userRepository.Value;
+        public IChatRepository Chat => _chatRepository.Value;
+        public IChatMemberRepository ChatMember => _chatMemberRepository.Value;
+        public IMessageRepository Message => _messageRepository.Value;
 
         public void Save() => _context.SaveChanges();
     }
