1b519109e8ad933f472d73f25f694f11febdd325
Vla1mer <vla1mer8@gmail.com>
2026-04-14 21:40:05 +0200

    Fixed ChatFor and MessageFor

--- Services/DataTransferObjects/MessageForUpdateDto.cs
+++ Services/DataTransferObjects/MessageForUpdateDto.cs
@@ -6,6 +6,6 @@ namespace Services.DataTransferObjects
     {
         [Required(ErrorMessage = "Content is a required field.")]
         [MaxLength(2000, ErrorMessage = "Maximum length for Content is 2000 characters.")]
-        public string Content { get; init; } = null!;
+        public string? Content { get; init; }
     }
 }
